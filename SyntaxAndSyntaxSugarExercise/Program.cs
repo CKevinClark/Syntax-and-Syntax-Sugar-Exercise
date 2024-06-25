bb82963596153eed562a3f219fc734ce3ad87789
Kevin Clark <kevinclarkengineer@gmail.com>
2024-06-24 22:10:38 -0400

    This one was particularly fun!

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+
+             var randomNumber = random.Next();
+
+            var answer = 965674; //var inferred typing 
+
+            var response = (answer < randomNumber) ? $" less than {randomNumber}" : $" greater than or equal to {randomNumber}"; //ternary operator and inferred typing with Interpolated String true and false scopes
+            
+            Console.WriteLine($" You have failed, Duckman! For the answer is indeed {response}"); //interpolated string 
+            
         }
     }
 }
